e3ea5e72cad0c4efe3b2df817cfee01095788d01
Peter Freedman <frdmanp@gmail.com>
2024-12-29 17:11:01 -0500

    cell changes and formatting - made the cell constructor public so that some T<Cell[]> being used in some Maze<T> can create its cells. Marked it with a warning to not use independently. - removed unnecessary forward/friend declarations - remembered to format

--- maze.hpp
+++ maze.hpp
@@ -17,11 +17,11 @@ the algorithm makes use of this header.
 
 #pragma once
 
-#include <iostream> 
+#include <concepts>
+#include <iostream>
+#include <memory>
 #include <sstream>
 #include <type_traits>
-#include <memory>
-#include <concepts>
 
 class Cell;
 
@@ -30,23 +30,19 @@ class Cell;
     and that index returns a reference to a cell, it is a valid
     type
 */
-template <typename T>
+template<typename T>
 concept CanMaze = requires (T const& maze) {
   { maze[0] } -> std::same_as<Cell&>;
 };
-
-
-template<CanMaze Mazeable = std::unique_ptr<Cell[]>>
-class Maze;
 
 //cells have 4 walls (they are squares).
 //talks about which wall of a cell to view/change
 enum class Wall
 {
-    TOP = 0,
-    BOTTOM = 1,
-    LEFT = 2,
-    RIGHT = 3
+  TOP = 0,
+  BOTTOM = 1,
+  LEFT = 2,
+  RIGHT = 3
 };
 
 //there are two cells in a byte
@@ -54,13 +50,13 @@ enum class Wall
 //is a matter of addition (see Cell::getSide() for more)
 enum class Side
 {
-    RIGHT = 0,
-    LEFT = 4
+  RIGHT = 0,
+  LEFT = 4
 };
 
 //============================== Cell Definition ==============================
 
-    /** Represents a pair of two cells in a maze. 
+/** Represents a pair of two cells in a maze. 
         Because each would only have been four bits, two are packed together. 
         The struct is still called a cell for readability, 
         but in reality it will always be a cell pair. These cells 
@@ -84,109 +80,109 @@ enum class Side
         In other words, opened walls are connected to a maze and closed walls form
         the walls of the traversible paths in a maze. 
     */
-    class Cell{    
+class Cell
+{
 
-        friend std::ostream& operator<< (std::ostream& os, const Cell &c);
-        template<CanMaze Mazeable> friend class Maze;
+  std::uint8_t pair : 8;
 
-        std::uint8_t pair : 8;
-        
-        //default ctor
-        Cell ()
-        :pair(0)
-        {}
+public:
+  //default ctor - do not invoke manually
+  Cell () : pair (0) {}
 
-        //============================ Cell Accessors ============================
+private:
+  //============================ Cell Accessors ============================
 
-        //interprets the cell as an integer
-        inline int val () const { return pair; }
+  //interprets the cell as an integer
+  inline int
+  val () const
+  {
+    return pair;
+  }
 
-        //interprets the requested side of the cell as an integer
-        inline int side_val (Side l_or_r) const 
-        { 
-            return (pair >> int(l_or_r)) & 0b1111; 
-        }
+  //interprets the requested side of the cell as an integer
+  inline int
+  side_val (Side l_or_r) const
+  {
+    return (pair >> int (l_or_r)) & 0b1111;
+  }
 
-        //returns true if the provided cell wall is opened, false o/w
-        inline bool dir_val(Side l_or_r, Wall wall) const
-        { 
-            return (pair >> (int(l_or_r) + int(wall))) & 0b1; 
-        }
+  //returns true if the provided cell wall is opened, false o/w
+  inline bool
+  dir_val (Side l_or_r, Wall wall) const
+  {
+    return (pair >> (int (l_or_r) + int (wall))) & 0b1;
+  }
 
-        //============================= Cell Mutators =============================
+  //============================= Cell Mutators =============================
 
-        // Sets direction dir to true (removes that wall from the cell)
-        inline void setDirection (Side l_or_r, Wall wall) 
-        { 
-            pair |= (0b1 << (int(l_or_r) + int(wall))); 
-        }
+  // Sets direction dir to true (removes that wall from the cell)
+  inline void
+  setDirection (Side l_or_r, Wall wall)
+  {
+    pair |= (0b1 << (int (l_or_r) + int (wall)));
+  }
 
-        //Flips the state of the bit corresponding to dir
-        inline void updateDirection (Side l_or_r, Wall wall) 
-        { 
-            pair ^= (0b1 << (int(l_or_r) + int(wall))); 
-        }
-    };
+  //Flips the state of the bit corresponding to dir
+  inline void
+  updateDirection (Side l_or_r, Wall wall)
+  {
+    pair ^= (0b1 << (int (l_or_r) + int (wall)));
+  }
 
-    /** output stream operator overload for a cell
-        prints the left and right cells as ints separated by spaces
-        eg:
-        take the cell whose left side has its top and left walls opened 
-        and whose right side has its top and right walls opened:
+  /** output stream operator overload for a cell
+            prints the left and right cells as ints separated by spaces
+            eg:
+            take the cell whose left side has its top and left walls opened 
+            and whose right side has its top and right walls opened:
 
-        left      right
-                |
-                |
-                |
-        _________|_________
+            left      right
+                    |
+                    |
+                    |
+            _________|_________
 
-        according to the values of Side::LEFT, Side::TOP, and Side::RIGHT,
-        the left cell should have the bit pattern   0101 
-        the right cell should have the bitt pattern 1001
+            according to the values of Side::LEFT, Side::TOP, and Side::RIGHT,
+            the left cell should have the bit pattern   0101 
+            the right cell should have the bitt pattern 1001
 
-        this means that using the output stream on this cell would result in:
-        5 9
-        being printed
+            this means that using the output stream on this cell would result in:
+            5 9
+            being printed
     */
-    inline std::ostream& 
-    operator<< (std::ostream& os, const Cell &c)
-    {
-        os << c.side_val(Side::LEFT) << ' ' << c.side_val(Side::RIGHT);
-        return os;
-    }
+  friend std::ostream&
+  operator<< (std::ostream& os, const Cell& c)
+  {
+    os << c.side_val (Side::LEFT) << ' ' << c.side_val (Side::RIGHT);
+    return os;
+  }
+};
 
 //============================= End Cell Definition ============================
-
 
 template<CanMaze Mazeable>
 class Maze
 {
-    
 
-    Mazeable mz; 
-    int len;
-    int wid;
+  Mazeable mz;
+  int len;
+  int wid;
 
 public:
-    //force a maze to have dimensions
-    Maze () = delete; 
+  //force a maze to have dimensions
+  Maze () = delete;
 
-    //dimension ctor, creates a numRows x numCols maze
-    Maze (int numRows, int numCols)
-    :len(numRows), wid(numCols)
+  //dimension ctor, creates a numRows x numCols maze
+  Maze (int numRows, int numCols) : len (numRows), wid (numCols)
+  {
+    //because smart pointers don't use default ctors
+    if constexpr (std::is_same<Mazeable, std::unique_ptr<Cell[]>> ())
     {
-        //because smart pointers don't use default ctors
-        if constexpr (std::is_same<Mazeable, std::unique_ptr<Cell[]>>())
-        {
-            mz = std::make_unique<Cell[]>((numRows * numCols) / 2);
-        }
-        else if constexpr (std::is_same<Mazeable, std::shared_ptr<Cell[]>>())
-        {
-            mz = std::make_shared<Cell[]>((numRows * numCols) / 2);
-        }
-        else 
-        {
-            mz = Mazeable((numRows * numCols) / 2);
-        }
+      mz = std::make_unique<Cell[]> ((numRows * numCols) / 2);
     }
+    else if constexpr (std::is_same<Mazeable, std::shared_ptr<Cell[]>> ())
+    {
+      mz = std::make_shared<Cell[]> ((numRows * numCols) / 2);
+    }
+    else { mz = Mazeable ((numRows * numCols) / 2); }
+  }
 };
